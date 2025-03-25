e3c8ead01c8a98bd829b83d299687d4a22d7f8a8
tangfire <52628216@qq.com>
2025-03-25 22:11:35 +0800

    good night

---
+++ demo/user/rpc/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "Easy-GoZero/demo/user/rpc/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
